b207f63f2cbd6dfba143fa966705d2859512614d
student <isupov_evgenii@mail.ru>
2024-02-27 21:47:04 +0300

    27.02

--- djangoBigProject/mainApp/views.py
+++ djangoBigProject/mainApp/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from mainApp.services.productService import GetAllProducts, FindProductById
-from mainApp.services.toppingService import GetAllTopping
+from mainApp.services.toppingService import GetAlltopping
 from mainApp.services.cartService import GetCart as GC, AddProductInCart
+from mainApp.EmailSander import EmailSender
 
 # Create your views here.
 def Main(request):
@@ -9,10 +10,10 @@ def Main(request):
     return render(request, 'index.html', {'products':products})
 def Product(request, product_id):
     product = FindProductById(product_id)
-    toppings = GetAllTopping()
+    toppings = GetAlltopping()
     return render(request, 'product.html', {"product":product,"toppings":toppings})
 def AddInCart(request,product_id):
-    AddProductInCart(product_id):
+    AddProductInCart(product_id)
     return redirect('main')
 def GetCart(request):
     cart = GC()
@@ -21,3 +22,16 @@ def GetCart(request):
         "summ":cart.GetSum()
     }
     return render(request, 'cart.html', context)
+
+def SendEmail(request):
+    if request.method == "POST":
+        email = request.POST.get("email")
+        cart = GC()
+        message = ""
+        message += str(cart.GetProducts())+"\n"
+        message += str(cart.GetSum)+"\n"
+        emailSender = EmailSender(email)
+        emailSender.SendEmail("Заказ ", message)
+    return redirect('main')
+
+
